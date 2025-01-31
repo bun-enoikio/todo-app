48a162adf77f28f958fa868fbb09ef88455f3ce9
bun ly <lbunrith@gmail.com>
2025-01-31 10:57:15 -0800

    57f4 (#10)

--- scripts/github-action/delete-worker.sh
+++ scripts/github-action/delete-worker.sh
@@ -6,5 +6,27 @@ if [ -z "$1" ]; then
 fi
 
 BRANCH_NAME="$1"
+WORKERS=$(curl -s -X GET "https://api.cloudflare.com/client/v4/accounts/$CF_ACCOUNT_ID/workers/scripts" \
+-H "Authorization: Bearer $API_TOKEN" \
+-H "Content-Type: application/json" | jq -r '.result[]?.id | grep "$BRANCH_NAME"')
 
+if [ -z "$WORKERS" ]; then
+  echo "No worker found for branch $BRANCH_NAME"
+  exit 1
+fi
+
+MAIN_WORKER="$BRANCH_NAME"
+SERVICE_WORKER=($(echo "$WORKERS" | grep -v "^$MAIN_WORKER"))
+
+echo "Deleting worker $MAIN_WORKER"
+
+for worker in "${SERVICE_WORKER[@]}"; do
+  if [ -z "$worker" ]; then
+    echo "Deleting worker $worker"
+    curl -s -X DELETE "https://api.cloudflare.com/client/v4/accounts/$CF_ACCOUNT_ID/workers/scripts/$worker" \
+    -H "Authorization: Bearer $API_TOKEN" \
+    -H "Content-Type: application/json" | jq .
+
+  fi
+done
 echo "Branch Name $BRANCH_NAME is detected!"
