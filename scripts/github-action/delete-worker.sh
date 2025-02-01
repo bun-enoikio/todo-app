974cb4714c1c6a6ae15f8639887eed58e3b409e4
bun ly <lbunrith@gmail.com>
2025-01-31 16:34:37 -0800

    57f4 (#17)
    * 57f4
    
    * 57f4
    
    * 57f4
    
    * 57f4

--- scripts/github-action/delete-worker.sh
+++ scripts/github-action/delete-worker.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # Check if a branch name is provided
 
-# CF_ACCOUNT_ID="00e038df407fbb93b230bd5bea02c778"
-# API_TOKEN="t48OO40SPir-7H_dd8ZWMaONqln7SA-V0uSSWlPF"
+# Ensure environment variables are set
+if [ -z "$CF_ACCOUNT_ID" ] || [ -z "$CF_API_TOKEN" ]; then
+  echo "CF_ACCOUNT_ID and CF_API_TOKEN must be set"
+  exit 1
+fi
+
 if [ -z "$1" ]; then
   echo "Usage: $0 <branch name>"
   exit 1
