a9c67265d1d27d344856c08b7c2636d35c54f431
lbunrith <lbunrith@gmail.com>
2025-01-31 16:19:53 -0800

    dfdf

--- scripts/github-action/delete-worker.sh
+++ scripts/github-action/delete-worker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Check if a branch name is provided
 
-CF_ACCOUNT_ID="00e038df407fbb93b230bd5bea02c778"
-API_TOKEN="t48OO40SPir-7H_dd8ZWMaONqln7SA-V0uSSWlPF"
+# CF_ACCOUNT_ID="00e038df407fbb93b230bd5bea02c778"
+# API_TOKEN="t48OO40SPir-7H_dd8ZWMaONqln7SA-V0uSSWlPF"
 if [ -z "$1" ]; then
   echo "Usage: $0 <branch name>"
   exit 1
@@ -10,7 +10,7 @@ fi
 
 BRANCH_NAME="$1"
 WORKERS=$(curl -s -X GET "https://api.cloudflare.com/client/v4/accounts/$CF_ACCOUNT_ID/workers/scripts" \
--H "Authorization: Bearer $API_TOKEN" \
+-H "Authorization: Bearer $CF_API_TOKEN" \
 -H "Content-Type: application/json" | jq -r '.result[]?.id')
 
 echo "Fetched workers: $WORKERS"  # Debug information
@@ -29,7 +29,7 @@ for worker in "${SERVICE_WORKER[@]}"; do
   if [ -n "$worker" ]; then
     echo "Deleting worker $worker"
     curl -s -X DELETE "https://api.cloudflare.com/client/v4/accounts/$CF_ACCOUNT_ID/workers/scripts/$worker" \
-    -H "Authorization: Bearer $API_TOKEN" \
+    -H "Authorization: Bearer $CF_API_TOKEN" \
     -H "Content-Type: application/json" | jq .
   fi
 done
